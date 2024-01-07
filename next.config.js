3583d57659907f932997d759d645cd16dfc57fde
Mustafa Zorlu <mzorlu3579@gmail.com>
2024-01-08 01:27:02 +0300

    updated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 const nextConfig = {
+    output: "export",
     sassOptions: {
         includePaths: [path.join(__dirname, "styles")],
-        output: "export",
     },
 };
 
