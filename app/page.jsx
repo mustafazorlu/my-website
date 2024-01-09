2a3c639fcfc8a4376f0eb360747224bdbc5f4be2
Mustafa Zorlu <mzorlu3579@gmail.com>
2024-01-09 12:16:34 +0300

    loading2

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        setTimeout(() => setLoading(false), 1000);
+        setTimeout(() => setLoading(false), 1500);
     }, []);
     return (
         <>
