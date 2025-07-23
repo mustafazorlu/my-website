05b10abb6813be9313e0d63c652d5deb8fd242fd
mustafazorlu <mzorlu3579@gmail.com>
2025-07-23 22:20:21 +0300

    build changes #5

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
     sassOptions: {
         includePaths: ["./styles"],
+    },
+    images: {
+        unoptimized: true, // eğer next/image kullanıyorsan şart
     },
 };
 
